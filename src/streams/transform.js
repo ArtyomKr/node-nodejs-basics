9ab02255e71f7959c75de4ccf9173531c680d32b
ArtyomKr <ArtyomKr@users.noreply.github.com>
2022-06-03 20:41:07 +0300

    feat: finish all streams tasks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,16 @@
+import * as readline from 'readline';
+
 export const transform = async () => {
-    // Write your code here 
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  process.stdout.write('Type text and press Enter to reverse it: \n');
+
+  rl.on('line', (input) => {
+    process.stdout.write(input.split('').reverse().join('') + '\n');
+  });
 };
+
+transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,23 @@
+import path, { dirname } from 'path';
+import fs from 'fs';
+import * as readline from 'readline';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const PATH = path.join(__dirname, './files', 'fileToWrite.txt');
+
 export const write = async () => {
-    // Write your code here 
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  const writeStream = fs.createWriteStream(PATH);
+  process.stdout.write('Type text and press Enter to write it in the file: \n');
+
+  rl.on('line', (input) => {
+    writeStream.write(input + '\n');
+  });
 };
