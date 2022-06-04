f14118cbaaeadfbebac35a95d2645e178711ca0e
ArtyomKr <ArtyomKr@users.noreply.github.com>
2022-06-04 10:43:27 +0300

    feat: finish all zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,17 @@
+import { createGzip } from 'zlib';
+import path, { dirname } from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const OUTPUT_PATH = path.join(__dirname, './files', 'archive.gz');
+const PATH = path.join(__dirname, './files', 'fileToCompress.txt');
+
 export const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const source = fs.createReadStream(PATH);
+  const destination = fs.createWriteStream(OUTPUT_PATH);
+  source.pipe(gzip).pipe(destination);
 };
