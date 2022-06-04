5534e92c024b689f7c6a20d1594f8c10db6e7099
ArtyomKr <ArtyomKr@users.noreply.github.com>
2022-06-04 14:16:32 +0300

    feat: finish all wt tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,28 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
 export const performCalculations = async () => {
-    // Write your code here
+  const createWorkerPromise = (n) => {
+    const worker = new Worker('./worker.js', {  workerData: {n: n} });
+    return new Promise((resolve) => {
+      const result = { status: 'error', data: null };
+      worker.on('message', message => {
+        result.status = 'resolved';
+        result.data = message;
+        resolve(result);
+      });
+      worker.on('error', () => resolve(result));
+      worker.on('exit', (code) => {
+        if (code !== 0)
+          resolve(result);
+      });
+    });
+  };
+
+  const emptyArr = new Array(cpus().length).fill(null);
+  const promises = emptyArr.map((item, pos) => createWorkerPromise(pos + 10));
+
+  return Promise.all(promises);
 };
+
+console.log(await performCalculations());
