60f061f624b4e033a8aad6ce2bf27b985549f9ce
ArtyomKr <ArtyomKr@users.noreply.github.com>
2022-06-04 14:36:41 +0300

    feat: rewrite transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,14 +1,15 @@
-import * as readline from 'readline';
+import { Transform } from 'stream';
 
 export const transform = async () => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
   process.stdout.write('Type text and press Enter to reverse it: \n');
 
-  rl.on('line', (input) => {
-    process.stdout.write(input.split('').reverse().join('') + '\n');
+  const reverse = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, chunk.toString().split('').reverse().join('') + '\n\n');
+    },
   });
+
+  process.stdin.pipe(reverse).pipe(process.stdout);
 };
+
+await transform();
