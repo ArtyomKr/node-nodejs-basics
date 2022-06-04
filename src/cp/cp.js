46b18f3f3afafc0e2cae390cff08931872ae71d4
ArtyomKr <ArtyomKr@users.noreply.github.com>
2022-06-04 14:16:08 +0300

    feat: finish cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,9 @@
+import { spawn } from 'child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = spawn('node', [ './files/script.js', ...args]);
+  child.stdout.on('data', (data) => process.stdout.write(data));
+  process.stdin.pipe(child.stdin);
 };
+
+spawnChildProcess([ 'a', 'b']);
